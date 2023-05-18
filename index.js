ee94228894a08fc05599c97325b429774745524a
Waleed Kamal <55249888+WaledKamal@users.noreply.github.com>
2023-05-18 09:21:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo00000!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+const data = require("./data.json");
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json(data);
+});
+
+app.post("/add", (req, res) => {
+  const newData = req.body;
+  const allData = [...data, newData];
+  fs.writeFile("./data.json", JSON.stringify(allData), (err) => {
+    if (err) throw err;
+    res.send("Data Added!");
+  });
+});
+
+app.post("/update", (req, res) => {
+  const newData = req.body;
+  const allData = data.filter((item) => item.key !== newData.key);
+  allData.push(newData);
+  fs.writeFile("./data.json", JSON.stringify(allData), (err) => {
+    if (err) throw err;
+    res.send("Data updated!");
+  });
+});
+
+app.post("/delete", (req, res) => {
+  const newData = req.body;
+  const allData = data.filter((item) => item.key !== newData.key);
+  fs.writeFile("./data.json", JSON.stringify(allData), (err) => {
+    if (err) throw err;
+    res.send("Data updated!");
+  });
+});
+
+app.listen(80, () => {
+  console.log("App listening on port 80!");
+});
