d4be07b9477c45e68397e02648ce41c23f7cd450
Waleed Kamal <55249888+WaledKamal@users.noreply.github.com>
2023-08-26 17:02:11 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
  
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://dev-user:Sportify%402023@dev.9rummt9.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://waleedkamal:25102001@dev.9rummt9.mongodb.net/?retryWrites=true&w=majority";
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
